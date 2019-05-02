4a46a0cedce07042decdba00bc3bbfbc58c32782
Bytekeeper <therealdante@gmail.com>
2019-05-02 15:29:27 +0200

    Added 2019 Season 1 map pool

--- src/main/kotlin/org/bytekeeper/ctr/Maps.kt
+++ src/main/kotlin/org/bytekeeper/ctr/Maps.kt
@@ -7,7 +7,8 @@ import org.springframework.stereotype.Component
 class Maps {
 
     val mapPools = listOf(
-            poolSscait
+            poolSscait,
+            SCMapPool.`2019Season1`
     )
 
     fun getMap(fileName: String) = SCMap.of(fileName)
@@ -33,6 +34,16 @@ data class SCMapPool(val name: String, val maps: List<SCMap>) {
                 "sscai/(4)Python.scx",
                 "sscai/(4)Circuit Breaker.scx",
                 "sscai/(2)Heartbreak Ridge.scx"
+        ).map(SCMap.Companion::of))
+
+        val `2019Season1` = SCMapPool("2019Season1", listOf(
+                "2019Season1/(3)Medusa 2.2_iCCup.scx",
+                "2019Season1/(4)Fighting Spirit.scx",
+                "2019Season1/(4)Ground_Zero_2.0_iCCup.scx",
+                "2019Season1/(2)Cross Game.scx",
+                "2019Season1/(2)Overwatch(n).scx",
+                "2019Season1/(4)CircuitBreakers1.0.scx",
+                "2019Season1/(4)Colosseum 2.0_iCCup.scx"
         ).map(SCMap.Companion::of))
     }
 }
