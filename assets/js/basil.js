ee0759f009a5a391dabb162d1f207b9755929e8b
Bytekeeper <therealdante@gmail.com>
2019-07-25 18:19:11 +0200

    Link from front page; opponent graph

--- assets/js/basil.js
+++ assets/js/basil.js
@@ -2,14 +2,14 @@ import html from "../lib/html-template-tag.js"
 
 window.html = html;
 
-window.basil = function(basil) {
-	basil.formatDateTime = function(epochSecond) {
+window.basil = function (basil) {
+	basil.formatDateTime = function (epochSecond) {
 		return moment.unix(epochSecond).format("YYYY.MM.DD hh:mm a");
 	};
-	basil.formatDate = function(epochSecond) {
+	basil.formatDate = function (epochSecond) {
 		return moment.unix(epochSecond).format("YYYY.MM.DD");
 	};
-	basil.racecol = function(race) {
+	basil.racecol = function (race) {
 		switch (race) {
 			case "PROTOSS":
 			case "P":
@@ -27,11 +27,11 @@ window.basil = function(basil) {
 				return "race_unknown";
 		}
 	};
-	basil.percentFormat = function(value, digits) {
-		return new Intl.NumberFormat(undefined, { style: "percent", minimumFractionDigits: digits || 2}).format(value);
+	basil.percentFormat = function (value, digits) {
+		return new Intl.NumberFormat(undefined, { style: "percent", minimumFractionDigits: digits || 2 }).format(value);
 	};
 
 	Chart.plugins.unregister(ChartDataLabels);
 };
 
-$(function() {window.basil(window.basil);})
+$(function () { window.basil(window.basil); })
