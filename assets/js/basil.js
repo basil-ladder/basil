fd2e18a65e97fd0cc47e9f60330d00aba230a64f
Bytekeeper <therealdante@gmail.com>
2020-09-03 20:43:24 +0200

    #45 Some small fixes and WIP rank number sorting by rank+ELO

--- assets/js/basil.js
+++ assets/js/basil.js
@@ -51,7 +51,19 @@ window.basil = function (basil) {
 	basil.percentFormat = function (value, digits) {
 		return new Intl.NumberFormat(undefined, { style: "percent", minimumFractionDigits: digits || 2 }).format(value);
 	};
-	basil.sortByRank = function (data) {
+	function rankValue(rank) {
+		switch (rank) {
+			case "S": return 0;
+			case "A": return 1;
+			case "B": return 2;
+			case "C": return 3;
+			case "D": return 4;
+			case "E": return 5;
+			case "F": return 6;
+			case "UNRANKED": return 99999;
+		}
+	}
+	basil.sortByRank = function (data, useRank) {
 		data.sort(function (a, b) {
 			if (a.enabled != b.enabled) {
 				if (a.enabled) return -1; else return 1;
@@ -60,6 +72,7 @@ window.basil = function (basil) {
 			let bPlayed = b.won + b.lost;
 			if (aPlayed < 30 && bPlayed >= 30) return 1;
 			if (bPlayed < 30 && aPlayed >= 30) return -1;
+			if (useRank && rankValue(a.rank) != rankValue(b.rank)) return rankValue(a.rank) - rankValue(b.rank);
 			if (a.rating !== b.rating) return b.rating - a.rating;
 			if (a.winRate !== b.winRate) return b.winRate - a.winRate;
 			return b.won - a.won;
